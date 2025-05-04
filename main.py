3d663592f038d8c81835b6730c547c463d2e6356
worshix <worshiplivingstone@gmail.com>
2025-05-04 23:53:21 +0200

    added middleware and loaded templates and static files

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, BackgroundTasks, Request, File, UploadFile, Form, Query
+from utils.database import *
+from utils.password_handler import *
+from utils.email_handler import *
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.sessions import SessionMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+from typing import Optional
+import shutil
+import os
 from contextlib import asynccontextmanager
 import uvicorn
+
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -25,20 +39,22 @@ async def lifespan(app: FastAPI):
     print("Application is shutting down...")
 
 app = FastAPI(lifespan=lifespan)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+## Student endpoints
 
-@app.on_event("startup")
-async def startup_event():
-    """
-    This function runs when the application starts.
-    Add initialization code here.
-    """
-    print("Application is starting up...")
-    # Add your initialization code here
-    # Examples:
-    # - Database connections
-    # - Load ML models
-    # - Initialize cache
-    # - Load configuration
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "PUT", "POST", "DELETE"],
+    allow_headers=["*"]
+)
+
+app.add_middleware(
+    SessionMiddleware,
+    secret_key="5b9cf554803dde92a4ef38053b63823294d2145dbfd26ce2fd04f88663bd8082",
+)
 
 @app.get("/")
 def read_root():
--- utils/database.py
+++ utils/database.py
@@ -1,7 +1,9 @@
 from tinydb import TinyDB, Query
 from utils.generate_uuid import generate_id
 from utils.get_date import get_date
+import os
 
+os.makedirs("./database", exist_ok=True)
 students = TinyDB("./database/students.json")
 
 Item = Query()
