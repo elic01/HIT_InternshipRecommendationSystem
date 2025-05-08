23776c61efd1f936de227f28bab03eaa3151fd50
worshix <worshiplivingstone@gmail.com>
2025-05-08 05:31:50 +0200

    clean Up now in progress

--- main.py
+++ main.py
@@ -295,7 +295,13 @@ async def recommendations(request: Request):
     student_id = request.session.get("student_id")
     if not student_id:
         return RedirectResponse(url="/login", status_code=303)
-    return templates.TemplateResponse("recommendations.html", {"request": request})
+    student = get_student_by_id(student_id)
+    if not student:
+        return RedirectResponse(url="/login", status_code=303)
+    student["department"] = get_department_by_id(student["departmentId"])
+    jobs = await get_jobs(discipline=student["department"]["name"], location="Zimbabwe")
+    return templates.TemplateResponse("recommendations.html", {"request": request, "student": student, "jobs": jobs})
+
 
 @app.get("/error", response_class=HTMLResponse)
 async def error(request: Request):
